842a19b4bce4febfbcc09a18c40862fbf5c9fb53
jlake70 <jlake7021@gmail.com>
2024-04-28 12:40:53 -0400

    Added examples of how to use variables

--- feature.py
+++ feature.py
@@ -2,3 +2,42 @@ def greet_coding_temple():
         print("Ahoy, Coding Teemple Navigators! Ready for today's adventure?")
         if __name__ == "__main__":
                 greet_coding_temple()
+
+# Assigning a variable and printing that variable
+full_name= "Josh Lake";
+print(full_name)
+
+#Assigning two variables with the same name
+myName="Joshua";
+myname="Lake";
+print(myName)
+print(myname)
+
+fav_color="blue";
+print(fav_color)
+
+#Assigning multiple values in a sinle line
+fruit1, fruit2, fruit3="apple", "bananas","cherry";
+print(fruit1)
+print(fruit2)
+print(fruit3)
+
+#Assign an integer float and a string to 3 variables
+integer_var=38;
+float_var=3.5;
+string_var="Hello World";
+print(integer_var, type(integer_var))
+print(float_var, type(float_var))
+print(string_var, type(string_var))
+
+#Assigning the type function
+mystery_variable="1234"
+variable_type= type(mystery_variable)
+print(variable_type)
+
+#Assign a variable as integer and reassign variable to a string
+dynamic_var=10
+print(dynamic_var, type(dynamic_var))
+
+dynamic_var="Python is great"
+print(dynamic_var, type(dynamic_var))
